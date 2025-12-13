d91bf538965ca59336875bb4417bddbc2232273c
Prahlada Aniruddha <manurprahlada@gmail.com>
2025-12-13 09:33:26 +0530

    final changes

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,7 @@ require('dotenv').config()
 const port = process.env.PORT || 3090
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: ['https://portfolio-website-git-main-prahladas-projects.vercel.app', 'https://portfolio-website-gamma-khaki-27.vercel.app', process.env.FRONTEND_URL || 'http://localhost:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}))
+app.use(cors())
 const multer = require("multer")
 //-----------------------------------------------------------------------------------------
 const configDB = require('./config/configDB');
