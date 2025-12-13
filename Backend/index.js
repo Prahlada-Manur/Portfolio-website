cd9b7b90206d3cdebc17b2381e13466ce229d69f
Prahlada Aniruddha <manurprahlada@gmail.com>
2025-12-13 09:26:27 +0530

    index link changes

--- Backend/index.js
+++ Backend/index.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 3090
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || '*',
-  credentials: true
+  origin: ['https://portfolio-website-git-main-prahladas-projects.vercel.app', 'https://portfolio-website-gamma-khaki-27.vercel.app', process.env.FRONTEND_URL || 'http://localhost:5173'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }))
 const multer = require("multer")
 //-----------------------------------------------------------------------------------------
